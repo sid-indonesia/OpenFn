4759c44941d61500501b91f088cddab2c99aee13
Levi <78334846+muhammad-levi@users.noreply.github.com>
2022-03-22 11:48:37 +0700

    muhammad.levi@sid-indonesia.org's change on OpenFn.org

--- expressions/kobo-to-fhir-server.js
+++ expressions/kobo-to-fhir-server.js
@@ -52,7 +52,7 @@ post(sourceValue("$.configuration.resource")(state) + "Patient", {
   ),
   headers: {
     "Content-Type": "application/fhir+json",
-    "Authorization": sourceValue("$.configuration.accessToken")
+    "Authorization": sourceValue("$.configuration.accessToken"),
   },
 });
 
