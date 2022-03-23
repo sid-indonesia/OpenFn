5c7f740d2b463b99e697cad0656cc6f0a5f0345e
Levi <muhammad.levi@sid-indonesia.org>
2022-03-23 17:05:28 +0700

    Affected Issue(s): Map `state.data` to FHIR resources
    https://www.notion.so/Back-End-Map-resources-in-kobo-to-fhir-server-js-test-using-OpenFn-devtools-dd90663c250c421abc9211fcd07eccf4
    
    What this commit has achieved:
    1. Map FHIR resources from an example `state.json`
    2. TODO post the transformed data as transaction bundle into a FHIR server

--- expressions/kobo-to-fhir-server.js
+++ expressions/kobo-to-fhir-server.js
@@ -1,17 +1,56 @@
 // Build "patient" resource
 fn(state => {
+  console.log(dataValue('patient/name'),
+    dataValue('patient/name')(state));
   const patient = {
     resourceType: 'Patient',
     identifier: [
       {
         use: 'usual',
         system: 'https://fhir.kemkes.go.id/id/nik',
-        value: dataValue("['patient/identifier_NIK']"),
+        value: dataValue('patient/identifier_NIK')(state),
+      },
+    ],
+    name: [
+      {
+        use: "official",
+        text: dataValue('patient/name')(state),
+      },
+    ],
+    gender: "female",
+    birthDate: dataValue('patient/birth_date')(state),
+    address: [
+      {
+        use: "home",
+        text: dataValue('patient/address')(state),
+      },
+    ],
+    contact: [
+      {
+        name: {
+          use: "official",
+          text: dataValue('patient/husband_name')(state),
+        },
+        gender: "male",
+        extension: [
+          {
+            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate",
+            valueDate: dataValue('patient/husband_birth_date')(state),
+          },
+          {
+            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier",
+            valueIdentifier: {
+              use: "usual",
+              system: "https://fhir.kemkes.go.id/id/nik",
+              value: dataValue('patient/husband_identifier_NIK')(state),
+            },
+          },
+        ],
       },
     ],
   };
 
-  return { data: { patient } };
+  return { ...state, data: { patient } };
 });
 
 // Build "observation" resource
@@ -25,6 +64,12 @@ fn(state => {
   console.log('these are the prepared FHIR resources', state.data);
   return state;
 });
+
+// @Taylor, how to attach the `state` from previous `fn()` in this `post()` as its request body?
+// Currently it's the `state` before those `fn()`s.
+post("", state);
+// TODO post as FHIR transaction bundle
+// https://www.hl7.org/fhir/http.html#transaction
 
 // TODO: @Levi, once you're happy with the resources, try sending them
 // to a particular URL and view the server response in the output.json file.
