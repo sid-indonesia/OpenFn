bc76835e054c4982856157b9948d7a3854b9b683
Levi <78334846+muhammad-levi@users.noreply.github.com>
2022-03-24 16:43:45 +0700

    muhammad.levi@sid-indonesia.org's change on OpenFn.org

--- expressions/kobo-to-fhir-server.js
+++ expressions/kobo-to-fhir-server.js
@@ -7,41 +7,41 @@
 //       {
 //         use: 'usual',
 //         system: 'https://fhir.kemkes.go.id/id/nik',
-//         value: dataValue('patient/identifier_NIK'),
+//         value: dataValue('patient_ID/patient_identifier_NIK'),
 //       },
 //     ],
 //     name: [
 //       {
 //         use: 'official',
-//         text: dataValue('patient/name'),
+//         text: dataValue('patient_ID/patient_name'),
 //       },
 //     ],
 //     gender: 'female',
-//     birthDate: dataValue('patient/birth_date'),
+//     birthDate: dataValue('patient_ID/patient_dob'),
 //     address: [
 //       {
 //         use: 'home',
-//         text: dataValue('patient/address'),
+//         text: dataValue('patient_ID/patient_address'),
 //       },
 //     ],
 //     contact: [
 //       {
 //         name: {
 //           use: 'official',
-//           text: dataValue('patient/husband_name'),
+//           text: dataValue('patient_ID/partner_name'),
 //         },
 //         gender: 'male',
 //         extension: [
 //           {
 //             url: 'https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate',
-//             valueDate: dataValue('patient/husband_birth_date'),
+//             valueDate: dataValue('patient_ID/partner_dob'),
 //           },
 //           {
 //             url: 'https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier',
 //             valueIdentifier: {
 //               use: 'usual',
 //               system: 'https://fhir.kemkes.go.id/id/nik',
-//               value: dataValue('patient/husband_identifier_NIK'),
+//               value: dataValue('patient_ID/partner_identifier_NIK'),
 //             },
 //           },
 //         ],
@@ -111,41 +111,42 @@ post(sourceValue('$.configuration.resource')(state) + 'Patient', {
       {
         use: 'usual',
         system: 'https://fhir.kemkes.go.id/id/nik',
-        value: dataValue('patient/identifier_NIK'),
+        value: dataValue('patient_ID/patient_identifier_NIK'),
+        
       },
     ]),
     field('name', [
       {
         use: 'official',
-        text: dataValue('patient/name'),
+        text: dataValue('patient_ID/patient_name'),
       },
     ]),
     field('gender', 'female'),
-    field('birthDate', dataValue('patient/birth_date')),
+    field('birthDate', dataValue('patient_ID/patient_dob')),
     field('address', [
       {
         use: 'home',
-        text: dataValue('patient/address'),
+        text: dataValue('patient_ID/patient_address'),
       },
     ]),
     field('contact', [
       {
         name: {
           use: 'official',
-          text: dataValue('patient/husband_name'),
+          text: dataValue('patient_ID/partner_name'),
         },
         gender: 'male',
         extension: [
           {
             url: 'https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate',
-            valueDate: dataValue('patient/husband_birth_date'),
+            valueDate: dataValue('patient_ID/partner_dob'),
           },
           {
             url: 'https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier',
             valueIdentifier: {
               use: 'usual',
               system: 'https://fhir.kemkes.go.id/id/nik',
-              value: dataValue('patient/husband_identifier_NIK'),
+              value: dataValue('patient_ID/partner_identifier_NIK'),
             },
           },
         ],
