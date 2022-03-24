f1d72c1b1dacdbd948f78f13b12fa340d10d6f2b
Levi <muhammad.levi@sid-indonesia.org>
2022-03-24 12:02:07 +0700

    Affected Issue(s): Test double ()() in OpenFn.org
    https://www.notion.so/Back-End-Map-resources-in-kobo-to-fhir-server-js-test-using-OpenFn-devtools-dd90663c250c421abc9211fcd07eccf4
    
    What this commit has achieved:
    1. Test double-parentheses for the "Authorization" header, using the string from the `$.configuration.accessToken`

--- expressions/kobo-to-fhir-server.js
+++ expressions/kobo-to-fhir-server.js
@@ -1,146 +1,148 @@
-// Build "patient" resource
-fn(state => {
-  console.log(dataValue('patient/name'),
-    dataValue('patient/name')(state));
-  const patient = {
-    resourceType: 'Patient',
-    identifier: [
-      {
-        use: 'usual',
-        system: 'https://fhir.kemkes.go.id/id/nik',
-        value: dataValue('patient/identifier_NIK')(state),
-      },
-    ],
-    name: [
-      {
-        use: "official",
-        text: dataValue('patient/name')(state),
-      },
-    ],
-    gender: "female",
-    birthDate: dataValue('patient/birth_date')(state),
-    address: [
-      {
-        use: "home",
-        text: dataValue('patient/address')(state),
-      },
-    ],
-    contact: [
-      {
-        name: {
-          use: "official",
-          text: dataValue('patient/husband_name')(state),
-        },
-        gender: "male",
-        extension: [
-          {
-            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate",
-            valueDate: dataValue('patient/husband_birth_date')(state),
-          },
-          {
-            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier",
-            valueIdentifier: {
-              use: "usual",
-              system: "https://fhir.kemkes.go.id/id/nik",
-              value: dataValue('patient/husband_identifier_NIK')(state),
-            },
-          },
-        ],
-      },
-    ],
-  };
+// // Build "patient" resource
+// fn(state => {
+//   console.log(dataValue('patient/name'),
+//     dataValue('patient/name')(state));
+//   // @Taylor, why if without "(state)" it returns `[Function (anonymous)]` and write no value in the `output.json`?
 
-  return { ...state, data: { patient } };
-});
-
-// Build "observation" resource
-fn(state => {
-  const observation = {};
-
-  return { data: { ...state.data, observation } };
-});
-
-fn(state => {
-  console.log('these are the prepared FHIR resources', state.data);
-  return state;
-});
-
-// @Taylor, how to attach the `state` from previous `fn()` in this `post()` as its request body?
-// Currently it's the `state` before those `fn()`s.
-post("", state);
-// TODO post as FHIR transaction bundle
-// https://www.hl7.org/fhir/http.html#transaction
-
-// TODO: @Levi, once you're happy with the resources, try sending them
-// to a particular URL and view the server response in the output.json file.
-
-// post("Patient", {
-//   body: fields(
-//     field("resourceType", "Patient"),
-//     field("identifier", [
+//   const patient = {
+//     resourceType: 'Patient',
+//     identifier: [
 //       {
-//         use: "usual",
-//         system: "https://fhir.kemkes.go.id/id/nik",
-//         value: dataValue("['patient/identifier_NIK']"),
+//         use: 'usual',
+//         system: 'https://fhir.kemkes.go.id/id/nik',
+//         value: dataValue('patient/identifier_NIK')(state),
 //       },
-//     ]),
-//     field("name", [
+//     ],
+//     name: [
 //       {
 //         use: "official",
-//         text: dataValue("['patient/name']"),
+//         text: dataValue('patient/name')(state),
 //       },
-//     ]),
-//     field("gender", "female"),
-//     field("birthDate", dataValue("['patient/birth_date']")),
-//     field("address", [
+//     ],
+//     gender: "female",
+//     birthDate: dataValue('patient/birth_date')(state),
+//     address: [
 //       {
 //         use: "home",
-//         text: dataValue("['patient/address']"),
+//         text: dataValue('patient/address')(state),
 //       },
-//     ]),
-//     field("contact", [
+//     ],
+//     contact: [
 //       {
 //         name: {
 //           use: "official",
-//           text: dataValue("['patient/husband_name']"),
+//           text: dataValue('patient/husband_name')(state),
 //         },
 //         gender: "male",
 //         extension: [
 //           {
 //             url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate",
-//             valueDate: dataValue("['patient/husband_birth_date']"),
+//             valueDate: dataValue('patient/husband_birth_date')(state),
 //           },
 //           {
 //             url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier",
 //             valueIdentifier: {
 //               use: "usual",
 //               system: "https://fhir.kemkes.go.id/id/nik",
-//               value: dataValue("['patient/husband_identifier_NIK']"),
+//               value: dataValue('patient/husband_identifier_NIK')(state),
 //             },
 //           },
 //         ],
 //       },
-//     ])
+//     ],
+//   };
+
+//   return { ...state, data: { patient } };
+// });
+
+// // Build "observation" resource
+// fn(state => {
+//   const observation = {};
+
+//   return { data: { ...state.data, observation } };
+// });
+
+// fn(state => {
+//   console.log('these are the prepared FHIR resources', state.data);
+//   return state;
+// });
+
+// // @Taylor, how to attach the `state` from previous `fn()` in this `post()` as its request body?
+// // Currently it's the `state` before those `fn()`s.
+// post("", state);
+// // TODO post as FHIR transaction bundle
+// // https://www.hl7.org/fhir/http.html#transaction
+
+// // TODO: @Levi, once you're happy with the resources, try sending them
+// // to a particular URL and view the server response in the output.json file.
+
+post("Patient", {
+  body: fields(
+    field("resourceType", "Patient"),
+    field("identifier", [
+      {
+        use: "usual",
+        system: "https://fhir.kemkes.go.id/id/nik",
+        value: dataValue("['patient/identifier_NIK']"),
+      },
+    ]),
+    field("name", [
+      {
+        use: "official",
+        text: dataValue("['patient/name']")(state),
+      },
+    ]),
+    field("gender", "female"),
+    field("birthDate", dataValue("['patient/birth_date']")(state)),
+    field("address", [
+      {
+        use: "home",
+        text: dataValue("['patient/address']")(state),
+      },
+    ]),
+    field("contact", [
+      {
+        name: {
+          use: "official",
+          text: dataValue("['patient/husband_name']")(state),
+        },
+        gender: "male",
+        extension: [
+          {
+            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-birthDate",
+            valueDate: dataValue("['patient/husband_birth_date']")(state),
+          },
+          {
+            url: "https://fhir.kemkes.go.id/StructureDefinition/patient-contact-identifier",
+            valueIdentifier: {
+              use: "usual",
+              system: "https://fhir.kemkes.go.id/id/nik",
+              value: dataValue("['patient/husband_identifier_NIK']")(state),
+            },
+          },
+        ],
+      },
+    ])
+  ),
+  headers: {
+    "Content-Type": "application/fhir+json",
+    "Authorization": sourceValue("$.configuration.accessToken")(state),
+  },
+});
+
+// post("Observation", {
+//   body: fields(
+//     field("resourceType", "Observation"),
+//     field("status", "final"),
+//     field(
+//       "subject",
+//       field(
+//         "reference",
+//         (state) => `Patient/${dataValue("id")(state)}`
+//       )
+//     )
 //   ),
 //   headers: {
 //     "Content-Type": "application/fhir+json",
-//     "Authorization": sourceValue("$.configuration.accessToken"), // still not working
 //   },
 // });
-
-// // post("Observation", {
-// //   body: fields(
-// //     field("resourceType", "Observation"),
-// //     field("status", "final"),
-// //     field(
-// //       "subject",
-// //       field(
-// //         "reference",
-// //         (state) => `Patient/${dataValue("id")(state)}`
-// //       )
-// //     )
-// //   ),
-// //   headers: {
-// //     "Content-Type": "application/fhir+json",
-// //   },
-// // });
