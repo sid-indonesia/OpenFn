f87b0106a4a29978c0ad55f7a798381ccf373777
Levi <muhammad.levi@sid-indonesia.org>
2022-10-13 13:29:37 +0700

    Affected Issue(s):
    https://www.notion.so/sid-levi/WA-Biz-Developer-ETL-Developer-Process-data-from-FHIR-Server-to-Qontak-API-via-OpenFn-b8728797e7174d9d8aa6bccdfad3f329
    
    What this commit has achieved:
    1. Created script to send WA message via Qontak API with retries if 429

---
+++ expressions/bunda-to-qontak/send-wa-message-anc-reminder.js
@@ -0,0 +1,144 @@
+fn(async state => {
+  const jobName = '"ANC Visit Reminder"';
+
+  // The call to your API
+
+  async function callTheAPI(reqIndex, attempt = 0) {
+    console.log('Request ', jobName, ' Start:', reqIndex, `attempt:${attempt}`, new Date().toISOString());
+
+    try {
+      const finalState = await post(`${state.configuration.qontak.baseUrl}/v1/broadcasts/whatsapp`, {
+        body: {
+          "name": "Test OpenFn",
+          "message_template_id": `${state.configuration.qontak.testMessageTemplateId}`,
+          "contact_list_id": `${state.configuration.qontak.contactListId}`,
+          "channel_integration_id": `${state.configuration.qontak.whatsAppChannelIntegrationId}`,
+          "parameters": {
+            "body": [
+              {
+                "key": "1",
+                "value": "full_name"
+              },
+              {
+                "key": "2",
+                "value": "pregna_trimester"
+              },
+              {
+                "key": "3",
+                "value": "calc_gestational"
+              }
+            ]
+          }
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${state.configuration.qontak.tokenType} ${state.configuration.qontak.accessToken}`
+        }
+      })(state);
+
+      console.log('Request ', jobName, ' End:  ', reqIndex, `attempt:${attempt}`, finalState.response.status)
+
+      return finalState;
+    } catch (err) {
+      if (err.response) {
+        console.log('Request ', jobName, ' End:  ', reqIndex, `attempt:${attempt}`, err.response.status);
+
+        if (err.response.status === 429 || err.response.status === 422) {
+          return { ...state, response: err.response };
+        }
+      }
+
+      throw err;
+    }
+  }
+
+  // Ratelimit helpers
+
+  class TokenBucketRateLimiter {
+
+    reset() {
+      this.count = 0;
+      this.resetTimeout = null;
+    }
+
+    scheduleReset() {
+      if (!this.resetTimeout) {
+        this.resetTimeout = setTimeout(() => this.reset(), this.maxRequestWindowMS);
+      }
+    }
+
+    async acquireToken(fn) {
+      if (this.count === this.maxRequests) {
+        await nextTick();
+        this.scheduleReset();
+        await sleep(this.maxRequestWindowMS);
+        return this.acquireToken(fn);
+      }
+
+      this.count = this.count + 1;
+      await nextTick();
+      return fn();
+    }
+  }
+
+  function getMillisToSleep(retryHeaderString) {
+    let millisToSleep = Math.round(parseFloat(retryHeaderString) * 1000);
+    if (isNaN(millisToSleep)) {
+      millisToSleep = Math.max(0, new Date(retryHeaderString) - new Date());
+    }
+    return millisToSleep;
+  }
+
+  async function fetchAndRetryIfNecessary(callAPI, index, attempt = 0) {
+    const finalState = await callAPI(attempt);
+    if (finalState.response.status === 429) {
+      const retryAfter = finalState.response.headers["x-ratelimit-reset"]; // Qontak set this header
+      const millisToSleep = getMillisToSleep(retryAfter) + state.configuration.qontak.broadcastBulk.extraWaitMS;
+      console.log('❗ Retrying:  ', index, `attempt:${attempt + 1}`, 'at', retryAfter, 'sleep for', millisToSleep, 'ms');
+      await sleep(millisToSleep);
+      return fetchAndRetryIfNecessary(callAPI, index, attempt + 1);
+    } else if (finalState.response.status === 422
+      && attempt < 10
+      && finalState.response.data
+      && finalState.response.data.error.messages[0].toLowerCase() === "please wait a moment to send a new broadcast message.") {
+
+      const millisToSleep = 100;
+      console.log('❗ Retrying weird 422:  ', index, `attempt:${attempt + 1}`, 'sleep for', millisToSleep, 'ms');
+      await sleep(millisToSleep);
+      return fetchAndRetryIfNecessary(callAPI, index, attempt + 1);
+    }
+    return finalState;
+  }
+
+  // General Helpers
+
+  function sleep(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  }
+
+  function nextTick() {
+    return sleep(0);
+  }
+
+  function getArrayOfLength(length) {
+    return Array.from(Array(length).keys());
+  }
+
+
+
+  const tokenBucket = new TokenBucketRateLimiter({
+    maxRequests: state.configuration.qontak.broadcastBulk.maxRequests,
+    maxRequestWindowMS: state.configuration.qontak.broadcastBulk.maxRequestsWindowMS
+  });
+  tokenBucket.reset();
+
+  const promises = getArrayOfLength(state.configuration.qontak.broadcastBulk.numRequestsParallel).map(async (index) => (
+    fetchAndRetryIfNecessary(async (attempt = 0) => (
+      tokenBucket.acquireToken(() => callTheAPI(index, attempt))
+    ), 0, index)
+  ));
+
+  let finalStates = await Promise.all(promises);
+
+  return finalStates[0];
+});
