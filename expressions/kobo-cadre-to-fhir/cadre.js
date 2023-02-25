88028272b4b1e086a42dd948ea186c16efd622d2
Levi <78334846+muhammad-levi@users.noreply.github.com>
2023-02-26 01:22:20 +0700

    muhammad.levi@sid-indonesia.org's change on OpenFn.org

--- expressions/kobo-cadre-to-fhir/cadre.js
+++ expressions/kobo-cadre-to-fhir/cadre.js
@@ -191,9 +191,9 @@ get(`${state.configuration.resource}/RelatedPerson`,
   {
     query: {
       identifier: `https://fhir.kemkes.go.id/id/temp-identifier-mother-name-and-baby-name|` +
-        sourceValue('commonFunctions').trimSpacesTitleCase(sourceValue('koboData')[sourceValue('inputKey').required.motherName]).replace(/ /g, "_") +
+        sourceValue('commonFunctions')(state).trimSpacesTitleCase(sourceValue('koboData')(state)[sourceValue('inputKey')(state).required.motherName]).replace(/ /g, "_") +
         `-` +
-        sourceValue('commonFunctions').trimSpacesTitleCase(sourceValue('koboData')[sourceValue('$.inputKey').required.babyName]).replace(/ /g, "_"),
+        sourceValue('commonFunctions')(state).trimSpacesTitleCase(sourceValue('koboData')(state)[sourceValue('$.inputKey')(state).required.babyName]).replace(/ /g, "_"),
     },
     headers: {
       'content-type': 'application/fhir+json',
