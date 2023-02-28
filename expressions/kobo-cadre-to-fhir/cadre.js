4bd3ef0cc8f3fcf818df45fa81fc4e7b1f3956b1
Levi <muhammad.levi@sid-indonesia.org>
2023-02-28 11:47:10 +0700

    Affected Issue(s): Nested, nested array
    https://www.notion.so/sid-levi/JavaScript-Developer-Add-ability-to-merge-nested-array-in-a-nested-object-17359a4e01984de285964491ce18b36f?pvs=4
    
    What this commit has achieved:
    1. Call the method mergeNestedArray recursively
    2. Check `hasNestedArray` recursively

--- expressions/kobo-cadre-to-fhir/cadre.js
+++ expressions/kobo-cadre-to-fhir/cadre.js
@@ -50,6 +50,9 @@ fn(state => {
       }
     },
 
+    hasNestedArray: (value) => typeof value === 'object' && value !== null && !Array.isArray(value) &&
+      Object.values(value).some(v => Array.isArray(v) || state.commonFunctions.hasNestedArray(v)),
+
     mergeNestedArrayAndRemoveDuplicates: (obj1, obj2) => {
       const mergedObj = {};
 
@@ -60,43 +63,25 @@ fn(state => {
         if (Array.isArray(value1) && Array.isArray(value2)) {
           // If the property is an array, merge it and remove duplicates
           mergedObj[key] = state.commonFunctions.mergeArrayAndRemoveDuplicates(value1, value2);
-        } else {
-          // Otherwise, use the value from the second object (obj2) to overwrite the value in the first object (obj1)
+        } else if (state.commonFunctions.hasNestedArray(value2) || state.commonFunctions.hasNestedArray(value1)) {
+          // If the property is an object which contains array as some of the properties, call current method recursively
+          mergedObj[key] = state.commonFunctions.mergeNestedArrayAndRemoveDuplicates(value1, value2);
+        } else if (obj2.hasOwnProperty(key) && value2 !== null) {
+          // If present, use the value from the second object (obj2) to overwrite the value in the first object (obj1)
           mergedObj[key] = value2;
+        } else {
+          // Otherwise, keep the one retrieved from server (obj1)
+          mergedObj[key] = value1;
         }
       }
 
       return mergedObj;
     },
 
-    mergeResourceFromServerWithNewlyCompiledResource: (resourceFromServer, resourceNewlyCompiled) => {
-      const arrayKeys = Object.keys(resourceNewlyCompiled).filter(key => Array.isArray(resourceNewlyCompiled[key]));
-      const mergedArrays = {};
-      for (const key of arrayKeys) {
-        mergedArrays[key] = state.commonFunctions.mergeArrayAndRemoveDuplicates(resourceFromServer[key], resourceNewlyCompiled[key]);
-      }
-
-      const nestedArrayKeys = Object.keys(resourceNewlyCompiled).filter(key => {
-        const value = resourceNewlyCompiled[key];
-        return typeof value === 'object' && value !== null && !Array.isArray(value) && Object.values(value).some(v => Array.isArray(v));
-      });
-      const mergedNestedArrays = {};
-      for (const key of nestedArrayKeys) {
-        mergedNestedArrays[key] = state.commonFunctions.mergeNestedArrayAndRemoveDuplicates(resourceFromServer[key], resourceNewlyCompiled[key]);
-      }
-
-      return {
-        ...resourceFromServer,
-        ...resourceNewlyCompiled,
-        ...mergedArrays,
-        ...mergedNestedArrays
-      };
-    },
-
     mergeResourceIfFoundInServer: (state, newlyCompiledResource) => {
       if (state.data.hasOwnProperty('entry')) {
         const resourceFromServer = state.data.entry[0].resource;
-        const mergedResource = state.commonFunctions.mergeResourceFromServerWithNewlyCompiledResource(resourceFromServer, newlyCompiledResource);
+        const mergedResource = state.commonFunctions.mergeNestedArrayAndRemoveDuplicates(resourceFromServer, newlyCompiledResource);
         return mergedResource;
       } else {
         return newlyCompiledResource;
